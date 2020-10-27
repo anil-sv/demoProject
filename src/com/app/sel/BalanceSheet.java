7ecea465dbf2c4bafa03356f9c8c0cb90761b674
anil-sv <73514271+anil-sv@users.noreply.github.com>
2020-10-27 12:33:50 +0530

    Update BalanceSheet.java
    updated string

--- src/com/app/sel/BalanceSheet.java
+++ src/com/app/sel/BalanceSheet.java
@@ -2,6 +2,8 @@ package com.app.sel;
 
 public class BalanceSheet {
 	
+	int num = 100;
+	String name = "nokia";
 	public static void main(String[] args) {
 		System.out.println("Balance amount is :: ");
 	}
